367223e5d8689d012a2b330a628dc5cdf49d3588
Brayan8abo <brayan8abo@outlook.es>
2024-11-14 23:38:08 +0100

    comprobación user y password

--- app/src/main/java/com/example/proyectosclase/euroMillonRegistro.java
+++ app/src/main/java/com/example/proyectosclase/euroMillonRegistro.java
@@ -13,7 +13,8 @@ public class euroMillonRegistro extends AppCompatActivity {
     private EditText user;
     private EditText pass;
     private Button btnEntrar;
-
+    private String[] arrayUser = {"Brayan", "Pablo", "Noel", "Carolina", "Juan Jose", "Alejandra", "Cristian"};
+    private String[] arrayPass = {"12345", "12345", "12345", "12345", "12345", "12345", "12345"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,26 +23,29 @@ public class euroMillonRegistro extends AppCompatActivity {
 
         user = findViewById(R.id.user);
         pass = findViewById(R.id.pass);
-        Button btnEntrar = findViewById(R.id.btnEntrar);
+        btnEntrar = findViewById(R.id.btnEntrar);
 
         btnEntrar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String usuario = user.getText().toString();
                 String password = pass.getText().toString();
-                if (usuario.equals("Brayan") && password.equals("12345")) {
+
+                boolean isUser = false;
+                for (int i = 0; i < arrayUser.length; i++) {
+                    if (arrayUser[i].equals(usuario) && arrayPass[i].equals(password)) {
+                        isUser = true;
+                        break;
+                    }
+                }
+                if (isUser) {
                     setContentView(R.layout.euro_millon);
                 } else {
-                    Toast.makeText(euroMillonRegistro.this, "Usuario o contrasena incorrecta", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(euroMillonRegistro.this, "Usuario o contraseña incorrecta", Toast.LENGTH_SHORT).show();
                     user.setText("");
                     pass.setText("");
-                    setContentView(R.layout.euro_millon_registro);
-
                 }
-
             }
         });
-
     }
-
 }
